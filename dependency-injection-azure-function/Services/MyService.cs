4c2564143b1919af828db295c9fe00308d30e51d
Ty Corcoran <45568306+tcorcor1@users.noreply.github.com>
2021-12-11 20:48:40 -0500

    post: dependency-injection-azure-function

---
+++ dependency-injection-azure-function/Services/MyService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+
+namespace dependency_injection_azure_function
+{
+    public class MyService : IMyService
+    {
+        private IHttpClientFactory _httpClientFactory;
+
+        public MyService (IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public void DoWork ()
+        {
+            Console.WriteLine("I am doing work");
+        }
+    }
+}
---
+++ dependency-injection-azure-function/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(dependency_injection_azure_function.Startup))]
+
+namespace dependency_injection_azure_function
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure (IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient("NHL-API", c =>
+            {
+                c.BaseAddress = new Uri("https://statsapi.web.nhl.com");
+                c.DefaultRequestHeaders.Add("Accept", "application/json");
+            });
+
+            builder.Services.AddScoped<IMyService, MyService>();
+        }
+    }
+}
