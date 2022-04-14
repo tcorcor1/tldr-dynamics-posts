08a7de1a22f138457507a85725654713a5a1db95
Ty Corcoran <45568306+tcorcor1@users.noreply.github.com>
2022-04-14 07:56:45 -0400

    post: azure-synapse-sqlclient-console

---
+++ azure-synapse-sqlclient-console/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace azure_synapse_sqlclient_console
+{
+    internal class Program
+    {
+        private static void Main (string[] args)
+        {
+            #region setup appsettings
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false);
+            IConfiguration config = builder.Build();
+
+            #endregion setup appsettings
+
+            var dbConn = config.GetConnectionString("DbConnection");
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(dbConn))
+                {
+                    connection.Open();
+
+                    string query = @"SELECT TOP(5) * FROM [dataverse_tldrdynamics_unq3345a000000000000000].[dbo].[yyz_team];";
+                    using (SqlCommand sqlCommand = new SqlCommand(query, connection))
+                    using (SqlDataReader reader = sqlCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(String.Format("{0}", reader["yyz_team_name"]));
+                        };
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+    }
+}
