07c3ebec37f11da40ac7aff91257931d19b2f6ea
tcorcor1 <tyler.corcoran@yahoo.com>
2023-04-11 14:54:27 -0400

    post: d365-logging-alerts-application-insights

---
+++ d365-logging-alerts-application-insights/Tldr.Demo.PluginLoggingAlerts/Tldr.Demo.PluginLoggingAlerts/DemoPlugin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tldr.Common;
+
+namespace Tldr.Demo.PluginLoggingAlerts
+{
+	public class DemoPlugin : PluginBase
+	{
+		public override void ExecuteAction (ExecutionContext ctx)
+		{
+			ctx.Logger.LogInformation("TLDR DEMO: This is a log written to app insights");
+
+			ctx.Logger.LogError("TLDR DEMO: This is an error message written to app insights");
+		}
+	}
+}
