2ae4dd8d3572eaec38ce2b0d29f144437bb85d9d
tcorcor1 <tyler.corcoran@yahoo.com>
2024-02-12 14:12:37 -0500

    update-post: custom-exception-middleware-for-organization-service-faults

--- custom-exception-middleware-for-organization-service-faults/OrgServiceExceptionMiddleware/Middleware/ExceptionHandlerMiddleware.cs
+++ custom-exception-middleware-for-organization-service-faults/OrgServiceExceptionMiddleware/Middleware/ExceptionHandlerMiddleware.cs
@@ -9,57 +9,57 @@ using Microsoft.Azure.Functions.Worker.Http;
 
 namespace OrgServiceExceptionMiddleware
 {
-	public class ExceptionHandlerMiddleware : IFunctionsWorkerMiddleware
-	{
-		public async Task Invoke (FunctionContext context, FunctionExecutionDelegate next)
-		{
-			try
-			{
-				await next.Invoke(context);
-			}
-			catch (Exception exception)
-			{
-				var logger = context.GetLogger<ExceptionHandlerMiddleware>();
+  public class ExceptionHandlerMiddleware : IFunctionsWorkerMiddleware
+  {
+    public async Task Invoke(FunctionContext context, FunctionExecutionDelegate next)
+    {
+      try
+      {
+        await next.Invoke(context);
+      }
+      catch (Exception exception)
+      {
+        var logger = context.GetLogger<ExceptionHandlerMiddleware>();
 
-				var req = await context.GetHttpRequestDataAsync();
-				var res = req!.CreateResponse();
+        var req = await context.GetHttpRequestDataAsync();
+        var res = req!.CreateResponse();
 
-				switch (exception)
-				{
-					case BadHttpRequestException ex:
-						logger.LogError(exception.Message);
+        switch (exception)
+        {
+          case BadHttpRequestException ex:
+            logger.LogError(exception.Message);
 
-						req = await context.GetHttpRequestDataAsync();
-						res = req!.CreateResponse();
-						res.StatusCode = HttpStatusCode.InternalServerError;
+            req = await context.GetHttpRequestDataAsync();
+            res = req!.CreateResponse();
+            res.StatusCode = HttpStatusCode.BadRequest;
 
-						await res.WriteStringAsync(ex.Message);
-						context.GetInvocationResult().Value = res;
-						break;
+            await res.WriteStringAsync(ex.Message);
+            context.GetInvocationResult().Value = res;
+            break;
 
-					case FaultException<OrganizationServiceFault> ex:
-						logger.LogCritical(((FaultException<OrganizationServiceFault>)exception).Detail.Message);
+          case FaultException<OrganizationServiceFault> ex:
+            logger.LogCritical(((FaultException<OrganizationServiceFault>)exception).Detail.Message);
 
-						req = await context.GetHttpRequestDataAsync();
-						res = req!.CreateResponse();
-						res.StatusCode = HttpStatusCode.InternalServerError;
+            req = await context.GetHttpRequestDataAsync();
+            res = req!.CreateResponse();
+            res.StatusCode = HttpStatusCode.InternalServerError;
 
-						await res.WriteStringAsync("Org service fault. Please try again or contact an administrator");
-						context.GetInvocationResult().Value = res;
-						break;
+            await res.WriteStringAsync("Org service fault. Please try again or contact an administrator");
+            context.GetInvocationResult().Value = res;
+            break;
 
-					default:
-						logger.LogError(exception.Message);
+          default:
+            logger.LogError(exception.Message);
 
-						req = await context.GetHttpRequestDataAsync();
-						res = req!.CreateResponse();
-						res.StatusCode = HttpStatusCode.InternalServerError;
+            req = await context.GetHttpRequestDataAsync();
+            res = req!.CreateResponse();
+            res.StatusCode = HttpStatusCode.InternalServerError;
 
-						await res.WriteStringAsync("Internal service error. Please contact an administrator");
-						context.GetInvocationResult().Value = res;
-						break;
-				}
-			}
-		}
-	}
+            await res.WriteStringAsync("Internal service error. Please contact an administrator");
+            context.GetInvocationResult().Value = res;
+            break;
+        }
+      }
+    }
+  }
 }
